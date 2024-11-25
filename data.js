a53fcd625214f1ddcef39d589553ed82cdf95539
Jean Alvarez <mikaelatomato@gmail.com>
2024-11-24 21:45:32 -0500

    Create blog posts dinamically

---
+++ data.js
@@ -0,0 +1,10 @@
+export const blog = [
+    {
+        title: "The first step",
+        normalDate: "Sunday, November 24, 2024",
+        computedDate: "2024-11-24",
+        content:
+            "<h3>test</h3>"
+    }
+]
+
---
+++ script.js
@@ -0,0 +1,26 @@
+import { blog } from "./data.js";
+
+const main = document.querySelector("main");
+
+const createBlogPost = (post) => {
+    const article = document.createElement("article");
+    const heading = document.createElement("h2");
+    const time = document.createElement("time");
+    const head = document.createElement("div");
+    head.classList.add("head");
+    article.classList.add("blog-post");
+    heading.textContent = post.title;
+    time.textContent = post.normalDate;
+    time.setAttribute("datetime", post.computedDate);
+    const content = document.createElement("div");
+    content.innerHTML = post.content;
+    content.classList.add(".content");
+    head.append(heading, time);
+    article.append(head, content);
+    return article;
+}
+
+for (const post of blog) {
+    const blogArticle = createBlogPost(post);
+    main.append(blogArticle);
+}
