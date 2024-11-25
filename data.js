007ca56e653b2cf3b937b1d0b7f9a00741965930
Jean Alvarez <mikaelatomato@gmail.com>
2024-11-24 22:07:53 -0500

    Add first blog post

--- data.js
+++ data.js
@@ -4,7 +4,28 @@ export const blog = [
         normalDate: "Sunday, November 24, 2024",
         computedDate: "2024-11-24",
         content:
-            "<h3>test</h3>"
+            `<p>Welcome to the "UN-NEET", a kind of blog, kind of journal 
+            where I will share my fight against my lazy, conformist and always guilty self, aiming to 
+            become a web developer and an independent person.
+            </p>
+            
+            <p>I will be honest, this is not the first time I have tried to change for the better,
+            and every previous time it has ended in me just accomplishing a little work, and then quitting
+            for the next weeks or months, and it has been this way for a couple of years now. I am not
+            just a NEET, I am an "Elite NEET", I have chosen this kind of life for myself thinking that I would
+            be able to change at any moment, and that the change would be easy, that achieving what I wanted in life
+            would be something that would naturally happen without a lot of effort or trouble.
+            </p>
+
+            <p>But as you can tell, I was completely wrong. After you have lived long enough in the same way, day after day,
+            you start to develop habits, behaviours, thoughts and a specific mindset that are very hard to quit.
+            I have become an expert at watching the days pass by doing absolutely nothing. I can be bored out of my mind,
+            and I will still not feel the desire to do anything to change my situation. Why? because it feels comfortable,
+            because studying, working, being disciplined is painful, tiring and the rewards that come from that feel like a distant dream.
+            </p>
+            
+            
+            `
     }
 ]
 
